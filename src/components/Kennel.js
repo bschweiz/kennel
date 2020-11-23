0ce6563d42a13d4f8ad38a4e25c067be54e0c9c4
Benjamin Schweizer <bschweiz@gmail.com>
2020-11-23 10:49:54 -0600

    trying to get ch1 to work

---
+++ src/components/Kennel.js
@@ -0,0 +1,14 @@
+// import React from "react"
+// import './Kennel.css'
+
+// this is an SINGLE-LINE-FAT-ARROW-FUNCTION so it don't need a return statement
+export const Kennel = () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <small>Loving care when you're not there.</small>
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+    </>
+)
