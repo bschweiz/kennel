6815e606345b2674f5727e90a3e656411b801c95
Benjamin Schweizer <bschweiz@gmail.com>
2020-11-23 10:54:39 -0600

    still getting error on the npm start

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,14 +1,22 @@
-// import React from "react"
-// import './Kennel.css'
+import React from "react"
+import { Animal } from "./animal/Animal"
+import "./animal/Animal.css"
 
-// this is an SINGLE-LINE-FAT-ARROW-FUNCTION so it don't need a return statement
 export const Kennel = () => (
     <>
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
+
         <address>
             <div>Visit Us at the Nashville North Location</div>
             <div>500 Puppy Way</div>
         </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <Animal />
+            <Animal />
+            <Animal />
+        </article>
     </>
 )
